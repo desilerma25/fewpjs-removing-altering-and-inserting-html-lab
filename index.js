5cd0ada3cb2e5bd9908694d6be04f8454a319d60
Desiree Lerma <desi.lerma96@yahoo.com>
2021-01-29 00:26:12 -0600

    commit tree 707bdf34b14cc6430120427c23a43443ce859e63

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+main.remove(); // removes main tag
+const newHeader = document.createElement('h1'); // defines const, creates new element
+newHeader.id = "victory"; //gives new element an id of victory
+newHeader.innerHTML = "Desi is the champion"; // adds the string inside our HTML tag/element we created
